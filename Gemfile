2d932e65eaf2194c13a9b61ca2729cccca07da75
zuckeyM-17 <kazuki.matsumura@studist.jp>
2024-01-02 02:15:27 +0900

    introduce rubocop-rails https://github.com/rubocop/rubocop-rails

--- Gemfile
+++ Gemfile
@@ -38,6 +38,13 @@ gem "bootsnap", require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
+
+  gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'factory_bot_rails'
+  gem 'pry-byebug'
+  gem 'pry-rails'
+  gem 'rubocop'
+  gem 'rubocop-rails'
 end
 
 group :development do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,11 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.generators.after_generate do |files|
+    parsable_files = files.filter { |file| file.end_with?('.rb') }
+    unless parsable_files.empty?
+      system("bundle exec rubocop -A --fail-level=E #{parsable_files.shelljoin}", exception: true)
+    end
+  end
 end
