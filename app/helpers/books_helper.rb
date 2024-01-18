4de99001eb6f79bc4feb26a9008fd177b7c16c88
zuckeyM-17 <kazuki.matsumura@studist.jp>
2024-01-18 23:59:14 +0900

    book category

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,5 +1,14 @@
 module BooksHelper
   def book_category(category)
+    color_map = {
+      'engineering' => 'bg-green-100',
+      'management' => 'bg-blue-100',
+      'english' => 'bg-yellow-100',
+      'other' => 'bg-gray-100'
+    }
 
+    content_tag(:span, class: "rounded text-gray-900 #{color_map[category]} py-1 px-2") do
+      category
+    end
   end
 end
