055b420723494c966abf449b97310bc7dce6b916
zuckeyM-17 <kazuki.matsumura@studist.jp>
2024-01-23 03:14:24 +0900

    page title

--- app/helpers/shared_helper.rb
+++ app/helpers/shared_helper.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 module SharedHelper
+  def full_title(page_title)
+    base_title = 'Hayate'
+    if page_title.blank?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
   def sidebar_link_to(path, text)
     classes = 'flex items-center p-2 space-x-3 rounded-md'
     active = 'dark:bg-gray-800 dark:text-gray-50' if current_page?(path)
