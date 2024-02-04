bec82614a840bee44d761d4fac3a38cb085e86fe
zuckeyM-17 <kazuki.matsumura@studist.jp>
2024-02-05 03:11:04 +0900

    modify review tool

--- app/controllers/review_tools_controller.rb
+++ app/controllers/review_tools_controller.rb
@@ -2,7 +2,9 @@
 
 class ReviewToolsController < ApplicationController
   def show
-    @posts = Sizume::Posts.new.call(created_after: Time.zone.now.beginning_of_week)
+    @posts = Sizume::Posts.new.call(created_after: 7.days.ago)
     @posts = @posts.sort_by(&:createdAt).reverse.select { |p| p.visibility == 'ANYONE' }
+
+    @readings = Reading.in_progress
   end
 end
