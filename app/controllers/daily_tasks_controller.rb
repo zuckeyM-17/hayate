a692e8043817b96ca45ae31fc1a36715a19a4e5e
zuckeyM-17 <kazuki.matsumura@studist.jp>
2024-02-12 23:42:12 +0900

    last week

--- app/controllers/daily_tasks_controller.rb
+++ app/controllers/daily_tasks_controller.rb
@@ -3,14 +3,14 @@
 class DailyTasksController < ApplicationController
   def update
     task = DailyTask.find(params[:id])
-    task.update!(update_params)
+    task.update!(done: update_params[:done])
 
-    redirect_to root_path
+    redirect_to root_path(last_week: update_params[:last_week])
   end
 
   private
 
   def update_params
-    params.permit(:done)
+    params.permit(:done, :last_week)
   end
 end
