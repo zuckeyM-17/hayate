b82b132cad15f3d5e17a4e6f892e387a62508a97
zuckeyM-17 <kazuki.matsumura@studist.jp>
2024-07-06 22:34:55 +0900

    fix rubocop

--- app/controllers/api/links_controller.rb
+++ app/controllers/api/links_controller.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
-class Api::LinksController < ApplicationController
-  protect_from_forgery
+module Api
+  class LinksController < ApplicationController
+    protect_from_forgery
 
-  def create
-    url = link_params[:url]
-    title = Link.get_title(url) || url
-    Link.create!(title:, url:)
+    def create
+      url = link_params[:url]
+      title = Link.get_title(url) || url
+      Link.create!(title:, url:)
 
-    head :created
-  end
+      head :created
+    end
 
-  private
+    private
 
-  def link_params
-    params.require(:link).permit(:url)
+    def link_params
+      params.require(:link).permit(:url)
+    end
   end
 end
