f41c1ee5ff68ba7e16c10746bc79892d96c4d056
zuckeyM-17 <kazuki.matsumura@studist.jp>
2024-11-07 14:38:20 +0900

    feeds

---
+++ app/controllers/feeds_controller.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+class FeedsController < ApplicationController
+  def index
+    @feeds = current_user.feeds.order(created_at: :desc).page(params[:page])
+    @feed = current_user.feeds.new
+  end
+
+  def create
+    feed = FeedResolver.new(create_params[:url]).resolve
+    feed.user = current_user
+    feed.save!
+    redirect_to feeds_path
+  end
+
+  def show
+    @feed = current_user.feeds.find(params[:id])
+  end
+
+  def edit
+    @feed = current_user.feeds.find(params[:id])
+    @feed.title = edit_params[:title]
+    redirect_to feeds_path
+  end
+
+  def destroy
+    feed = current_user.feeds.find(params[:id])
+    feed.destroy!
+    redirect_to feeds_path
+  end
+
+  private
+
+  def create_params
+    params.require(:feed).permit(:url)
+  end
+
+  def edit_params
+    params.require(:feed).permit(:title)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   resources :daily_task_items, only: %i[index create destroy]
   resources :daily_task_sets, only: %i[index create]
   resources :daily_tasks, only: %i[update]
+  resources :feeds, only: %i[index show create edit destroy]
   resources :links, only: %i[index create destroy] do
     member do
       patch :read
