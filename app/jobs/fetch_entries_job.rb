313b65d36539e127fb7e2ff833b354a2cf6ad52b
zuckeyM-17 <kazuki.matsumura@studist.jp>
2024-11-10 22:19:11 +0900

    transaction

--- app/jobs/fetch_entries_job.rb
+++ app/jobs/fetch_entries_job.rb
@@ -7,8 +7,14 @@ class FetchEntriesJob < ApplicationJob
   def perform(feed_id)
     feed = Feed.find(feed_id)
     latest_entry = feed.entries.order(published_at: :desc).first
-    entries = FeedEntryFetcher.new(feed).fetch!(from: latest_entry&.published_at)
-    entries.each(&:save!)
-    feed.update!(fetched_at: Time.zone.now)
+    entries = if latest_entry.present?
+                FeedEntryFetcher.new(feed).fetch!(from: latest_entry.published_at)
+              else
+                FeedEntryFetcher.new(feed).fetch!
+              end
+    ApplicationRecord.transaction do
+      entries.each(&:save!)
+      feed.update!(fetched_at: Time.zone.now)
+    end
   end
 end
