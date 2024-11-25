f76de9de4c0a5d2f3d01a73014c8aed50c5d2828
zuckeyM-17 <k.matsumura0314@gmail.com>
2024-11-25 23:29:33 +0900

    move sidebar_link from helper to erb

--- app/helpers/shared_helper.rb
+++ app/helpers/shared_helper.rb
@@ -9,14 +9,4 @@ module SharedHelper
       "#{page_title} | #{base_title}"
     end
   end
-
-  def sidebar_link_to(path, icon, text)
-    active = 'dark:bg-gray-800 dark:text-gray-50' if current_page?(path)
-    tag.li class: "overflow-hidden h-10 flex items-center #{active}" do
-      link_to(path, class: 'flex items-center p-2 space-x-3 rounded-md') do
-        concat tag.span icon
-        concat tag.span " #{text}", class: 'invisible lg:visible'
-      end
-    end
-  end
 end
