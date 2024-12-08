f8a7667077ae656f88e17cbbdb4bf41642dbe35a
zuckeyM-17 <k.matsumura0314@gmail.com>
2024-12-09 02:56:29 +0900

    remove sizume

--- app/controllers/review_tools_controller.rb
+++ app/controllers/review_tools_controller.rb
@@ -2,9 +2,6 @@
 
 class ReviewToolsController < BaseController
   def show
-    @posts = Sizume::Posts.new.call(created_after: 7.days.ago)
-    @posts = @posts.sort_by(&:createdAt).reverse.select { |p| p.visibility == 'ANYONE' }
-
     @readings = current_user.readings.in_progress
   end
 end
