118e19546769da5e82a118eff71ff0ffb43e9f9a
zuckeyM-17 <k.matsumura0314@gmail.com>
2024-12-21 02:07:15 +0900

    chrome extension for word_search

--- chrome_extension/src/background.js
+++ chrome_extension/src/background.js
@@ -1,4 +1,5 @@
 const CREATE_LINK = 'create_link'
+const CREATE_WORD_SEARCH = 'create_word_search'
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
@@ -6,11 +7,17 @@ chrome.runtime.onInstalled.addListener(() => {
     title: "リンクを保存",
     contexts: ["all"]
   });
+
+  chrome.contextMenus.create({
+    id: CREATE_WORD_SEARCH,
+    title: '選択した "%s" を保存',
+    contexts: ["selection"]
+  })
 });
 
 chrome.contextMenus.onClicked.addListener(async (info, tab) => {
   if (info.menuItemId === CREATE_LINK) {
-    chrome.tabs.sendMessage(tab.id, { type: "create_link" });
+    chrome.tabs.sendMessage(tab.id, { type: CREATE_LINK });
 
     await fetch('CREATE_LINK_API_URL', {
       method: 'POST',
@@ -23,6 +30,22 @@ chrome.contextMenus.onClicked.addListener(async (info, tab) => {
     chrome.scripting.executeScript({
       target: {tabId: tab.id},
       args: ['リンクを保存しました！'],
+      function: args => {  alert(args) }
+    });
+  } else if (info.menuItemId === CREATE_WORD_SEARCH) {
+    chrome.tabs.sendMessage(tab.id, { type: CREATE_WORD_SEARCH });
+
+    await fetch('CREATE_WORD_SEARCH_API_URL', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ word_search: { word: info.selectionText } }),
+    });
+
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      args: [`"${info.selectionText}"を保存しました！`],
       function: args => {  alert(args) }
     });
   }
