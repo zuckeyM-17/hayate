6d1a645356259c4566f10523318ceed581082ff0
zuckeyM-17 <k.matsumura0314@gmail.com>
2024-12-20 23:17:29 +0900

    update daily_task simultaneously

--- app/controllers/daily_tasks_controller.rb
+++ app/controllers/daily_tasks_controller.rb
@@ -2,11 +2,10 @@
 
 class DailyTasksController < BaseController
   def update
-    task = current_user.daily_tasks.find(params[:id])
-    task.update!(done: update_params[:done])
+    @task = current_user.daily_tasks.find(params[:id])
+    @task.update!(done: update_params[:done])
 
-    daily_task_items = current_user.daily_task_items.enabled.order(:created_at)
-    render 'daily_task_sets/_date', locals: { daily_task_set: task.daily_task_set, daily_task_items: }
+    @daily_task_items = current_user.daily_task_items.enabled.order(:created_at)
   end
 
   private
