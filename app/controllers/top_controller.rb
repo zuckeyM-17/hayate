10bc07df9160744119bff6f9de915185fd980c5a
zuckeyM-17 <k.matsumura0314@gmail.com>
2024-12-24 23:59:40 +0900

    rubocop

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -5,7 +5,8 @@ class TopController < BaseController
   def index
     @daily_task_set = current_user.daily_task_sets.find_by(date: Time.zone.today)
     @daily_task_items = current_user.daily_task_items.enabled.order(:created_at)
-    @notes = current_user.notes.includes(%i[link link_note event event_note task task_note reading reading_note ]).today.order(created_at: :desc)
+    @notes = current_user.notes.includes(%i[link link_note event event_note task task_note reading
+                                            reading_note]).today.order(created_at: :desc)
     @tasks = current_user.tasks.today.todo.order(end_date: :asc)
     today_task_ids = @tasks.map(&:id)
     @weekly_tasks = current_user.tasks.this_week.todo.order(end_date: :asc).reject do |task|
