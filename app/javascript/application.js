590fdf9ef3a480f186a4792decb500e146ac3bd5
zuckeyM-17 <k.matsumura0314@gmail.com>
2025-01-03 22:55:59 +0900

    init stimulus

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+import "controllers"
 
 if ('serviceWorker' in navigator) {
     // Register the service worker
