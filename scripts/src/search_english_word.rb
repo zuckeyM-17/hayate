8906e6f5fc421526615ba55a9f1694a1cc0e33b4
zuckeyM-17 <k.matsumura0314@gmail.com>
2025-01-05 00:13:42 +0900

    polling in raycast

--- scripts/src/search_english_word.rb
+++ scripts/src/search_english_word.rb
@@ -14,9 +14,9 @@ require 'uri'
 require 'json'
 
 authorization_token = 'API_AUTHORIZATION_TOKEN' # Your API Authorization Token
-url = 'CREATE_WORD_SEARCH_API_URL' # Your API URL
+url = 'API_URL' # Your API URL
 
-uri = URI.parse(url)
+uri = URI.parse("#{url}/word_searches")
 request = Net::HTTP::Post.new(uri)
 request.content_type = 'application/json'
 request['Authorization'] = "Bearer #{authorization_token}"
@@ -27,6 +27,35 @@ request.body = {
   }
 }.to_json
 
+word_id = nil
 Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
-  http.request(request)
+  res = http.request(request)
+
+  if res.code != '200'
+    puts "Error: #{res.code}"
+    exit
+  end
+  word_id = JSON.parse(res.body)['word']['id']
 end
+
+ja_word = nil
+
+uri = URI.parse("#{url}/words/#{word_id}")
+request = Net::HTTP::Get.new(uri)
+request.content_type = 'application/json'
+request['Authorization'] = "Bearer #{authorization_token}"
+
+while(ja_word.nil?)
+  sleep 2
+  Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+    res = http.request(request)
+
+    if res.code != '200'
+      puts "Error: #{res.code}"
+      exit
+    end
+    ja_word = JSON.parse(res.body)['word']['ja']
+  end
+end
+
+puts "#{ARGV[0]}: #{ja_word}. Saved!!"
