0088cbadcdfe82935dda355db17cad48ed3c3825
zuckeyM-17 <k.matsumura0314@gmail.com>
2025-01-04 23:38:32 +0900

    chrome extension polling

--- chrome_extension/src/background.js
+++ chrome_extension/src/background.js
@@ -1,7 +1,7 @@
 const CREATE_LINK = 'create_link'
 const CREATE_WORD_SEARCH = 'create_word_search'
 
-async function getAuthorizationToken() {
+const getAuthorizationToken = async() => {
   return new Promise((resolve, reject) => {
     chrome.storage.sync.get(['hayateApiAuthorizationToken'], (result) => {
       if (chrome.runtime.lastError) {
@@ -13,6 +13,10 @@ async function getAuthorizationToken() {
     });
   })
 }
+
+const sleep = () => new Promise(resolve => {
+  setTimeout(() => resolve(), 3000);
+});
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
@@ -29,39 +33,68 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+  const alert = (text) => {
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      args: [text],
+      function: args => { alert(args) }
+    });
+  }
+
+  const errorAlert = () => {
+    alert('エラーが発生しました。')
+  }
+
   const authorizationToken = await getAuthorizationToken();
   const headers = {
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${authorizationToken}`
   };
+  const base_url = 'API_URL'
 
   if (info.menuItemId === CREATE_LINK) {
     chrome.tabs.sendMessage(tab.id, { type: CREATE_LINK });
 
-    await fetch('CREATE_LINK_API_URL', {
+    await fetch(`${base_url}/links`, {
       method: 'POST',
       headers,
       body: JSON.stringify({ link: { url: tab.url } }),
     });
 
-    chrome.scripting.executeScript({
-      target: {tabId: tab.id},
-      args: ['リンクを保存しました！'],
-      function: args => {  alert(args) }
-    });
+    alert('リンクを保存しました！')
   } else if (info.menuItemId === CREATE_WORD_SEARCH) {
     chrome.tabs.sendMessage(tab.id, { type: CREATE_WORD_SEARCH });
 
-    await fetch('CREATE_WORD_SEARCH_API_URL', {
+    const res = await fetch(`${base_url}/word_searches`, {
       method: 'POST',
       headers,
       body: JSON.stringify({ word_search: { word: info.selectionText } }),
     });
 
-    chrome.scripting.executeScript({
-      target: {tabId: tab.id},
-      args: [`"${info.selectionText}"を保存しました！`],
-      function: args => {  alert(args) }
-    });
+    if (!res.ok) {
+      errorAlert()
+      return
+    }
+
+    const json = await res.json()
+    const word_id = json.word.id
+
+    let ja_word = ''
+    while (ja_word === '') {
+      await sleep();
+      const res = await fetch(`${base_url}/words/${word_id}`, {
+        method: 'GET',
+        headers,
+      });
+      console.log('polling...')
+      if (!res.ok) {
+        errorAlert()
+        return
+      }
+
+      const json = await res.json()
+      ja_word = json.word.ja
+    }
+    alert(`"${info.selectionText}": ${ja_word} を保存しました！`)
   }
 })
