fc50fd4d51f5c316c033efc6df2b9bad5b998a95
zuckeyM-17 <k.matsumura0314@gmail.com>
2025-01-24 02:01:19 +0900

    fix

--- app/controllers/weekly_reviews_controller.rb
+++ app/controllers/weekly_reviews_controller.rb
@@ -10,7 +10,7 @@ class WeeklyReviewsController < BaseController
     @weekly_objective = current_user.weekly_objectives.current_week.first
     @weekly_review = @weekly_objective.build_weekly_review(weekly_review_params)
     if @weekly_review.save
-      redirect_to weekly_review_path, notice: "Weekly review saved"
+      redirect_to weekly_review_path, notice: 'Weekly review saved'
     else
       render :show
     end
